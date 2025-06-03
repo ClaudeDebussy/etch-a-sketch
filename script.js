21779ca294b0fefb548be47e4d4446b27bababc1
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-06-03 10:43:15 +0500

    Clean up createGrid function

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const container = document.querySelector("#container");
 function createGrid(x){
     for (let i = 0; i < x * x; i++){
         const div = document.createElement("div");
-        //div.classList.add("box");
         container.appendChild(div);
         console.log(i + " has been created.");
     }
