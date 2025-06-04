42d3f89b31dc54eb84c47458064b5b52a2cd447d
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-06-04 11:44:00 +0500

    Refactor all functions, create clearBoard function

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const container = document.querySelector("#container");
 
-const button = document.querySelector("#newGame")
-button.addEventListener("click", setBoardSize)
+const button = document.querySelector("#newGame");
+button.addEventListener("click", newGame);
 
-function setBoardSize(){
+function newGame(){
+    clearboard();
+    createBoard();
+}
+
+function createBoard(){
+    const BOARD = 960;
+    const pixelsPerSide = getPixelsPerSide();
+    createGrid(BOARD/pixelsPerSide);
+    setSquareSize(pixelsPerSide)    
+}
+
+function getPixelsPerSide(){
     const BOARD = 960;
     let userInput = prompt('How many squares on each side?')
     userInput = Math.floor(userInput)
-    const squareSidesLength = BOARD/userInput
-    createGrid(userInput);
-    
-    const squares = document.querySelectorAll('#container > div')
-    console.log(squares);
-    for (let i = 0; i < squares.length; i++){
-        squares[i].style.width = `${squareSidesLength}px`;
-        squares[i].style.height = `${squareSidesLength}px`;
-    }
+    return BOARD/userInput
 }
 
 function createGrid(x){
@@ -23,6 +27,17 @@ function createGrid(x){
         const square = document.createElement("div");
         container.appendChild(square);
     }
-    
 }
 
+function setSquareSize(x){
+    const squares = document.querySelectorAll('#container > div')
+    console.log(squares);
+    for (let i = 0; i < squares.length; i++){
+        squares[i].style.width = `${x}px`;
+        squares[i].style.height = `${x}px`;
+    }
+}
+
+function clearboard(){
+    container.innerHTML = '';
+}
