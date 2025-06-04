0af45ca3bc28e11bf09d3341602d24ec0c3bdec2
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-06-04 11:19:01 +0500

    Create setBoardfSize function

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const container = document.querySelector("#container");
 
-function createGrid(x){
-    for (let i = 0; i < x * x; i++){
-        const div = document.createElement("div");
-        container.appendChild(div);
-        console.log(i + " has been created.");
+const button = document.querySelector("#newGame")
+button.addEventListener("click", setBoardSize)
+
+function setBoardSize(){
+    const BOARD = 960;
+    let userInput = prompt('How many squares on each side?')
+    userInput = Math.floor(userInput)
+    const squareSidesLength = BOARD/userInput
+    createGrid(userInput);
+    
+    const squares = document.querySelectorAll('#container > div')
+    console.log(squares);
+    for (let i = 0; i < squares.length; i++){
+        squares[i].style.width = `${squareSidesLength}px`;
+        squares[i].style.height = `${squareSidesLength}px`;
     }
 }
 
-createGrid(16);
+function createGrid(x){
+    for (let i = 0; i < x * x; i++){
+        const square = document.createElement("div");
+        container.appendChild(square);
+    }
+    
+}
+
